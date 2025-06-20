2fbdadcbfd056a711707345f5a28376a1499bf6b
BuddhikaBICT-UoR-FoT-6 <buddhikadarshan1475@gmail.com>
2025-06-20 09:30:00 +0530

    Add function docblocks to Models/User.php

--- Models/User.php
+++ Models/User.php
@@ -8,11 +8,21 @@ class User {
     private $conn;
     private $table = 'users';
 
+    /**
+     * User constructor.
+     * Initializes the database connection.
+     */
     public function __construct() {
         $db = new Database();
         $this->conn = $db->connect();
     }
 
+    /**
+     * Finds a user by their email address.
+     *
+     * @param string $email The email address of the user.
+     * @return array|false The user record as an associative array, or false if not found.
+     */
     public function findByEmail($email) {
         $query = "SELECT * FROM " . $this->table . " WHERE email = :email LIMIT 1";
         $stmt = $this->conn->prepare($query);
